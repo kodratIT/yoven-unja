3e686e565f4e4e50709f28247b15510e5c336a43
kodrat__ <kodratcoc@gmail.com>
2023-12-05 07:44:35 -0800

    create event

--- lib/event/event_profile_screen.dart
+++ lib/event/event_profile_screen.dart
@@ -1,10 +1,13 @@
+import 'dart:io';
+
 import 'package:yoven/helpers/theme/app_theme.dart';
-import 'package:yoven/helpers/utils/generator.dart';
 import 'package:yoven/helpers/widgets/my_container.dart';
-import 'package:yoven/helpers/widgets/my_spacing.dart';
 import 'package:yoven/helpers/widgets/my_text.dart';
 import 'package:flutter/material.dart';
 import 'package:lucide_icons/lucide_icons.dart';
+import 'package:yoven/helpers/widgets/my_button.dart';
+import 'package:yoven/helpers/widgets/my_text_style.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EventProfileScreen extends StatefulWidget {
   @override
@@ -12,177 +15,285 @@ class EventProfileScreen extends StatefulWidget {
 }
 
 class _EventProfileScreenState extends State<EventProfileScreen> {
+  bool _passwordVisible = false;
   late CustomTheme customTheme;
   late ThemeData theme;
+
+  final ImagePicker picker = ImagePicker();
+
+  XFile? imageFile;
 
   @override
   void initState() {
     super.initState();
     customTheme = AppTheme.customTheme;
     theme = AppTheme.theme;
-    desc = Generator.getDummyText(8);
   }
-
-  String? desc;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(
-      padding: MySpacing.top(MySpacing.safeAreaTop(context) + 20),
-      children: [
-        Column(
-          children: [
-            Container(
-              padding: MySpacing.fromLTRB(24, 8, 24, 0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(50)),
-                child: Image(
-                  image: AssetImage('./assets/images/profile/avatar_4.jpg'),
-                  width: 100,
-                  height: 100,
-                ),
-              ),
+        appBar: AppBar(
+          leading: InkWell(
+            onTap: () => Navigator.of(context).pop(),
+            child: Icon(
+              LucideIcons.chevronLeft,
+              size: 20,
+              color: theme.colorScheme.onBackground,
             ),
-            Container(
-              margin: MySpacing.top(8),
-              child: MyText.titleLarge("Martyn Rankin", fontWeight: 600),
-            ),
-          ],
+          ),
+          elevation: 0,
         ),
-        MyContainer.bordered(
-          margin: MySpacing.fromLTRB(24, 24, 24, 0),
-          padding: MySpacing.all(24),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                children: [
-                  MyText.bodySmall("My balance".toUpperCase(),
-                      fontSize: 12, fontWeight: 600, xMuted: true),
-                  MyText.bodyMedium("\$ 24", fontWeight: 600),
+        body: ListView(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              child: Column(
+                children: <Widget>[
+                  Stack(
+                    children: <Widget>[
+                      MyContainer.rounded(
+                        paddingAll: 0,
+                        width: 140,
+                        height: 140,
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        child: imageFile == null
+                            ? Image(
+                                image: AssetImage(
+                                  "./assets/images/profile/avatar_4.jpg",
+                                ),
+                                fit: BoxFit.fill,
+                              )
+                            : Image.file(
+                                File(
+                                  imageFile!.path,
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                      ),
+                      Positioned(
+                        bottom: 8,
+                        right: 8,
+                        child: InkWell(
+                          onTap: () async {
+                            imageFile = await picker.pickImage(
+                              source: ImageSource.gallery,
+                            );
+                            setState(() {});
+                          },
+                          child: MyContainer.rounded(
+                            border: Border.all(
+                                color: theme.scaffoldBackgroundColor,
+                                width: 2,
+                                style: BorderStyle.solid),
+                            paddingAll: 6,
+                            color: theme.colorScheme.primary,
+                            child: Icon(
+                              LucideIcons.pencil,
+                              size: 20,
+                              color: theme.colorScheme.onPrimary,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  MyText.titleLarge("Marcelina Willis",
+                      fontWeight: 600, letterSpacing: 0),
+                  MyText.titleSmall("UI Designer", fontWeight: 500),
                 ],
               ),
-              MyContainer(
-                padding: MySpacing.fromLTRB(16, 8, 16, 8),
-                color: theme.colorScheme.primary.withAlpha(28),
-                borderRadiusAll: 4,
-                child: Row(
-                  children: [
-                    Icon(
-                      LucideIcons.plus,
-                      color: theme.colorScheme.primary,
-                      size: 20,
-                    ),
-                    Container(
-                        margin: MySpacing.left(8),
-                        child: MyText.bodyMedium("Add Money",
-                            color: theme.colorScheme.primary, fontWeight: 600))
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-        Container(
-          margin: MySpacing.fromLTRB(24, 16, 24, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              MyText.bodySmall("INVITE",
-                  fontSize: 11.5, fontWeight: 600, xMuted: true),
-              Container(
-                margin: MySpacing.top(16),
-                child: Row(
-                  children: [
-                    Container(
-                      padding: MySpacing.all(8),
-                      decoration: BoxDecoration(
-                          color: theme.colorScheme.primary,
-                          borderRadius: BorderRadius.all(Radius.circular(8))),
-                      child: Icon(
-                        LucideIcons.plus,
-                        size: 22,
-                        color: theme.colorScheme.onPrimary,
-                      ),
-                    ),
-                    Container(
-                      margin: MySpacing.left(16),
-                      child: MyText.bodyMedium("Add Friend",
-                          color: theme.colorScheme.primary, fontWeight: 600),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: MySpacing.top(16),
-                child: singleInvite(
-                    image: './assets/images/profile/avatar_2.jpg',
-                    name: "Walter Gale",
-                    status: "Joined"),
-              ),
-              Container(
-                margin: MySpacing.top(16),
-                child: singleInvite(
-                    image: './assets/images/profile/avatar_3.jpg',
-                    name: "Tala Deacon",
-                    status: "Joined"),
-              ),
-              Container(
-                margin: MySpacing.top(16),
-                child: singleInvite(
-                    image: './assets/images/profile/avatar_4.jpg',
-                    name: "Isha Cameron",
-                    status: "Invite"),
-              )
-            ],
-          ),
-        )
-      ],
-    ));
-  }
-
-  Widget singleInvite(
-      {required String image, required String name, required String status}) {
-    return Row(
-      children: [
-        ClipRRect(
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-            child: Image(
-              image: AssetImage(image),
-              width: 40,
-              height: 40,
-            )),
-        Expanded(
-          child: Container(
-            margin: MySpacing.left(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                MyText.bodyMedium(name,
-                    color: theme.colorScheme.onBackground, fontWeight: 600),
-                MyText.bodySmall(
-                  status,
-                  fontWeight: 500,
-                  muted: true,
-                  color: theme.colorScheme.onBackground,
-                ),
-              ],
             ),
-          ),
-        ),
-        Container(
-          padding: MySpacing.fromLTRB(16, 8, 16, 8),
-          decoration: BoxDecoration(
-            color: theme.colorScheme.primary.withAlpha(40),
-            borderRadius: BorderRadius.all(Radius.circular(4)),
-          ),
-          child: MyText.bodySmall("+ \$9",
-              color: theme.colorScheme.primary,
-              letterSpacing: 0.3,
-              fontWeight: 700),
-        )
-      ],
-    );
-  }
+            Container(
+              padding: EdgeInsets.only(top: 36, left: 24, right: 24),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: TextFormField(
+                      style: MyTextStyle.bodyLarge(
+                          letterSpacing: 0.1,
+                          color: theme.colorScheme.onBackground,
+                          fontWeight: 500),
+                      decoration: InputDecoration(
+                        hintText: "Name",
+                        hintStyle: MyTextStyle.titleSmall(
+                            letterSpacing: 0.1,
+                            color: theme.colorScheme.onBackground,
+                            fontWeight: 500),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
+                            borderSide: BorderSide.none),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
+                            borderSide: BorderSide.none),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
+                            borderSide: BorderSide.none),
+                        filled: true,
+                        fillColor: customTheme.card,
+                        prefixIcon: Icon(
+                          LucideIcons.user,
+                        ),
+                        contentPadding: EdgeInsets.all(0),
+                      ),
+                      controller:
+                          TextEditingController(text: "Marcelina Willis"),
+                      textCapitalization: TextCapitalization.sentences,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: TextFormField(
+                      style: MyTextStyle.bodyLarge(
+                          letterSpacing: 0.1,
+                          color: theme.colorScheme.onBackground,
+                          fontWeight: 500),
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        hintStyle: MyTextStyle.titleSmall(
+                            letterSpacing: 0.1,
+                            color: theme.colorScheme.onBackground,
+                            fontWeight: 500),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
+                            borderSide: BorderSide.none),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
+                            borderSide: BorderSide.none),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
+                            borderSide: BorderSide.none),
+                        filled: true,
+                        fillColor: customTheme.card,
+                        prefixIcon: Icon(
+                          LucideIcons.mail,
+                        ),
+                        contentPadding: EdgeInsets.all(0),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      controller: TextEditingController(text: "nat@gmail.com"),
+                      textCapitalization: TextCapitalization.sentences,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: TextFormField(
+                      style: MyTextStyle.bodyLarge(
+                          letterSpacing: 0.1,
+                          color: theme.colorScheme.onBackground,
+                          fontWeight: 500),
+                      decoration: InputDecoration(
+                        hintText: "Phone",
+                        hintStyle: MyTextStyle.titleSmall(
+                            letterSpacing: 0.1,
+                            color: theme.colorScheme.onBackground,
+                            fontWeight: 500),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
+                            borderSide: BorderSide.none),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
+                            borderSide: BorderSide.none),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
+                            borderSide: BorderSide.none),
+                        filled: true,
+                        fillColor: customTheme.card,
+                        prefixIcon: Icon(
+                          LucideIcons.phone,
+                        ),
+                        contentPadding: EdgeInsets.all(0),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      textCapitalization: TextCapitalization.sentences,
+                      controller: TextEditingController(text: "091-987456321"),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: TextFormField(
+                      style: MyTextStyle.bodyLarge(
+                          letterSpacing: 0.1,
+                          color: theme.colorScheme.onBackground,
+                          fontWeight: 500),
+                      decoration: InputDecoration(
+                        hintText: "Change Password",
+                        hintStyle: MyTextStyle.titleSmall(
+                            letterSpacing: 0.1,
+                            color: theme.colorScheme.onBackground,
+                            fontWeight: 500),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
+                            borderSide: BorderSide.none),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
+                            borderSide: BorderSide.none),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8.0),
+                            ),
+                            borderSide: BorderSide.none),
+                        filled: true,
+                        fillColor: customTheme.card,
+                        prefixIcon: Icon(
+                          LucideIcons.lock,
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _passwordVisible
+                                ? LucideIcons.eye
+                                : LucideIcons.eyeOff,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _passwordVisible = !_passwordVisible;
+                            });
+                          },
+                        ),
+                        contentPadding: EdgeInsets.all(0),
+                      ),
+                      textCapitalization: TextCapitalization.sentences,
+                      obscureText: _passwordVisible,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 24),
+                    child: MyButton(
+                        onPressed: () {},
+                        borderRadiusAll: 4,
+                        elevation: 0,
+                        child: MyText.labelMedium("UPDATE",
+                            fontWeight: 600,
+                            color: theme.colorScheme.onPrimary,
+                            letterSpacing: 0.3)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+}
 }
